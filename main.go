17758df313215aed197f019180e7b5a68127aec9
Leonardo-lucas-pereira <leonardo_lucasp@hotmail.com>
2022-08-30 20:39:03 -0300

    Criação das Personalidades e retorno Json

--- main.go
+++ main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"go-rest-api/models"
 	"go-rest-api/routes"
 )
 
 func main() {
+	models.Personalidades = []models.Personalidade{
+		{Nome: "Nome 1", Historia: "Historia 1"},
+		{Nome: "Nome 2", Historia: "Historia 2"},
+		{Nome: "Nome 3", Historia: "Historia 3"},
+	}
+
 	fmt.Println("Iniciando o serviodor Rest com Go")
 	routes.HandleResquets()
 }
